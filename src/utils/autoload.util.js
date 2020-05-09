46d56a3b58ff0dd0c104ec32368efd885a615a59
Leonardo Rico <leonardo.ricog@gmail.com>
2020-05-09 15:52:34 -0500

    Updated

--- src/utils/autoload.util.js
+++ src/utils/autoload.util.js
@@ -2,22 +2,22 @@ const autoload = (path, params) => {
   try {
     switch (path) {
       case "routes":
-        const routes = require.context("../routes", false, /^((?!!).)*.js$/);
+        const routes = require.context("../routes", true, /^((?!!).)*.js$/);
         let routes_keys = routes.keys();
         //   console.log("files", routes_keys);
         return routes_keys.map(routes).map((x) => x.default);
       case "models":
-        const models = require.context("../models", false, /^((?!!).)*.js$/);
+        const models = require.context("../models", true, /^((?!!).)*.js$/);
         let models_keys = models.keys();
         //   console.log("files", models_keys);
         return models_keys.map(models);
       case "seeds":
-        const seeds = require.context("../seeds", false, /^((?!!).)*.js$/);
+        const seeds = require.context("../seeds", true, /^((?!!).)*.js$/);
         let seeds_keys = seeds.keys();
         //   console.log("files", seeds_keys);
         return seeds_keys.map(seeds).map((x) => x.default());
       case "sockets":
-        const sockets = require.context("../sockets", false, /^((?!!).)*.js$/);
+        const sockets = require.context("../sockets", true, /^((?!!).)*.js$/);
         let sockets_keys = sockets.keys();
         //   console.log("files", sockets_keys);
         return sockets_keys
@@ -27,6 +27,7 @@ const autoload = (path, params) => {
         break;
     }
   } catch (error) {
+    if (error?.code !== "MODULE_NOT_FOUND") console.log(error);
     return [];
   }
 };

4b54b1552d3e53dd6fb905fbac16053bb091f178
Leonardo Rico <leonardo.ricog@gmail.com>
2020-12-10 16:30:24 -0500

    Updated

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -6,48 +6,52 @@ import CopyPlugin from "copy-webpack-plugin";
 
 const src = path.resolve(__dirname, "src");
 
-const config = {
-  entry: [src],
-  output: {
-    path: path.join(__dirname, "dist"),
-    publicPath: "/",
-    filename: "app.js",
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new NodemonPlugin({
-      watch: [
-        path.resolve("./dist"),
-        path.resolve(".env"),
-        path.resolve("package.json"),
-      ],
-      ignore: ["./node_modules"],
-      verbose: true,
-      delay: "1000",
-    }),
-    new CopyPlugin({ patterns: [{ from: ".env" }] }),
-  ],
-  target: "node",
-  node: {
-    // Need this when working with express, otherwise the build fails
-    __dirname: false, // if you don't put this is, __dirname
-    __filename: false, // and __filename return blank or /
-  },
-  externals: [nodeExternals()], // Need this to avoid error when working with Express
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
+const config = (env, argv) => {
+  const isProduction = argv.mode === "production";
+
+  return {
+    entry: [src],
+    output: {
+      path: path.join(__dirname, "dist"),
+      publicPath: "/",
+      filename: isProduction ? "app.js" : "app.[contenthash].js",
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new NodemonPlugin({
+        watch: [
+          path.resolve("./dist"),
+          path.resolve(".env"),
+          path.resolve("package.json"),
+        ],
+        ignore: ["./node_modules"],
+        verbose: true,
+        delay: "1000",
+      }),
+      new CopyPlugin({ patterns: [{ from: ".env" }] }),
     ],
-  },
-  stats: {
-    warnings: false,
-  },
+    target: "node",
+    node: {
+      // Need this when working with express, otherwise the build fails
+      __dirname: false, // if you don't put this is, __dirname
+      __filename: false, // and __filename return blank or /
+    },
+    externals: [nodeExternals()], // Need this to avoid error when working with Express
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
+        },
+      ],
+    },
+    stats: {
+      warnings: false,
+    },
+  };
 };
 
 export default config;

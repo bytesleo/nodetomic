de13d9c32dad40f38429ab39367d7d41e4bdb7ac
leonardo <leonardo.ricog@gmail.com>
2021-07-12 00:34:26 -0500

    Added user middleware

--- src/libs/socketio.lib.js
+++ src/libs/socketio.lib.js
@@ -1,13 +1,14 @@
 import redisAdapter from 'socket.io-redis';
 // Constants
 import { WS, URI_WS_REDIS } from '@/constants/config.constant';
+// Business
+import UserBusiness from '@/business/users.business';
 // Utils
 import autoload from '@/utils/autoload.util';
 import { mws } from '@/utils/middleware.util';
 
 const io = require('socket.io')(WS);
 io.adapter(redisAdapter(URI_WS_REDIS));
-// Settings
 // io.eio.pingTimeout = 120000; // 2 minutes
 // io.eio.pingInterval = 5000; // 5 seconds
 
@@ -21,9 +22,9 @@ const connect = () =>
       // disconnect
       socket.on('disconnect', (reason) => {
         console.log(`❕Socket: client disconnected! (${socket.id}) ${reason}`);
-        // Remove user socket from db
         // UserBusiness.removeSocket(socket);
       });
+      // socket.set("pingTimeout", 63000);
       // autoload
       autoload.sockets(socket, io);
       resolve();
@@ -34,9 +35,10 @@ const connect = () =>
       socket.onAny(async (event) => {
         console.log(`Socket: event: ${event} (${socket.id})`);
       });
-      // Save user socket from db
+
+      const _next = await mws(socket, next);
       // UserBusiness.setSocket(socket);
-      return await mws(socket, next);
+      return _next;
     });
   });
 

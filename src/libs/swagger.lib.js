6095b38d5731326b7b1da15270cc0ac72b0e403f
leonardo <leonardo.ricog@gmail.com>
2021-08-03 02:33:36 -0500

    Renamed constants

--- src/libs/swagger.lib.js
+++ src/libs/swagger.lib.js
@@ -1,22 +1,30 @@
 import path from 'path';
 import swagger from 'express-jsdoc-swagger';
-
 // Constants
-import { NAME, MODE, HOST, PORT, DOMAIN } from '@/constants/config.constant';
+import {
+  PROJECT_NAME,
+  SERVER_HOSTNAME,
+  SERVER_PORT,
+  SWAGGER_HOSTNAME,
+  SWAGGER_API_DOCS
+} from '@/constants/config.constant';
 
 const create = async (app) => {
+  const servers = SWAGGER_HOSTNAME.split(',');
+
   const options = {
     info: {
-      title: NAME,
+      title: PROJECT_NAME,
       description: 'API description',
       version: '1.0.0'
     },
-    servers: [
-      {
-        url: MODE === 'development' ? `http://${HOST}:${PORT}` : DOMAIN,
-        description: MODE
-      }
-    ],
+    servers: servers.map((x) => ({
+      url:
+        x.includes('localhost') || x === ''
+          ? `http://${SERVER_HOSTNAME}:${SERVER_PORT}`
+          : x,
+      description: ''
+    })),
     security: {
       JWT: {
         type: 'apiKey',
@@ -28,14 +36,14 @@ const create = async (app) => {
     filesPattern: ['routes/**/[!!]*.js', 'utils/**/[!!]*.js'], // Glob pattern to find your jsdoc files (it supports arrays too ['./**/*.controller.js', './**/*.route.js'])
     swaggerUIPath: '/api-docs', // SwaggerUI will be render in this url. Default: '/api-docs'
     baseDir: path.resolve(__dirname, './../src'),
-    exposeSwaggerUI: MODE === 'development' ? true : false, // Expose OpenAPI UI. Default true
+    exposeSwaggerUI: SWAGGER_API_DOCS === 'true' ? true : false, // Expose OpenAPI UI. Default true
     exposeApiDocs: false, // Expose Open API JSON Docs documentation in `apiDocsPath` path. Default false.
     apiDocsPath: '/api-docs' // Open API JSON Docs endpoint. Default value '/v3/api-docs'.
   };
 
   swagger(app)(options);
 
-  return options.swaggerUIPath;
+  return options;
 };
 
 export { create };

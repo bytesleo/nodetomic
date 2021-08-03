9e51461781c3fc5f33939a169509c3f5ba59061b
leonardo <leonardo.ricog@gmail.com>
2021-08-03 02:30:56 -0500

    Updated dogs seed

--- src/seeds/!dogs.seed.js
+++ src/seeds/!dogs.seed.js
@@ -1,21 +1,24 @@
 // Models
 import TodoModel from '@/models/dogs.model';
 // Utils
-import { many } from '@/utils/seed.util';
+import { insert } from '@/utils/seed.util';
 // Data
 const data = [
   {
     name: 'Sparky',
-    race: 'Beagle'
+    race: 'Beagle',
+    user_id: '6108db02bb8ea9e69b2984a2'
   },
   {
     name: 'Zeus',
-    race: 'Chihuahua'
+    race: 'Chihuahua',
+    user_id: '6108db02bb8ea9e69b2984a2'
   },
   {
     name: 'Poseidon',
-    race: 'Bulldog'
+    race: 'Bulldog',
+    user_id: '6108db1b8d75624c1dd2ba2f'
   }
 ];
 
-export default async () => await many(TodoModel, data);
+export default async () => await insert(TodoModel, data);

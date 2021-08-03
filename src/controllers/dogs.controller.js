4f1e0c2538596260a729a26dbda84d2ebad43bfc
leonardo <leonardo.ricog@gmail.com>
2021-08-03 02:32:41 -0500

    Updated dogs controller

--- src/controllers/dogs.controller.js
+++ src/controllers/dogs.controller.js
@@ -1,24 +1,40 @@
 // Business
 import DogBusiness from '@/business/dogs.business';
 import { success, error } from '@/utils/helper.util';
+// Libs
+import validator from 'validator';
 
-const all = async (req, res) => {
+const getAll = async (req, res) => {
   try {
-    const data = await DogBusiness.all();
+    // Business logic
+    const data = await DogBusiness.getAll();
+    // Return success
     success(res, data);
   } catch (err) {
+    // Return error (if any)
     error(res, err);
   }
 };
 
-const allLogged = async (req, res) => {
+const getAllLogged = async (req, res) => {
   try {
-    const userId = req.user.id;
-    const data = await DogBusiness.allLogged(userId);
+    // Get current user id from session
+    const user_id = req.user.id;
+    // Validate data format
+    if (!validator.isMongoId(user_id)) {
+      throw {
+        code: 'ERROR_AUTH_4',
+        message: 'Invalid auth User id...'
+      };
+    }
+    // Business logic
+    const data = await DogBusiness.getAllLogged(user_id);
+    // Return success
     success(res, data);
   } catch (err) {
+    // Return error (if any)
     error(res, err);
   }
 };
 
-export default { all, allLogged };
+export default { getAll, getAllLogged };
